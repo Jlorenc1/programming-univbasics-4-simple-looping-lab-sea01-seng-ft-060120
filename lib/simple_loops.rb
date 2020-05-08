dabdad94eef717805005372b5b1476220edbddc5
Joshua Lorenc <joshua.lorenc@icloud.com>
2020-05-08 17:17:17 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,12 @@
-# Write your methods here
+def loop_message_five_times (string)
+  5.times do
+    puts string
+  end
+end
+
+def loop_message_n_times (n,string)
+  n.times do
+    puts string
+  end
+end
+
